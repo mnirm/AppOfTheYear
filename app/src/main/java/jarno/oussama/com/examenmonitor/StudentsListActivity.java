3257416b37ca1b9d6251fc29bdbc173535f96b5f
oussama Es-Salhi <oussie11@gmail.com>
2018-12-06 10:49:39 +0100

    adjusted students list and changed fab icon to a plus

--- app/src/main/java/jarno/oussama/com/examenmonitor/StudentsListActivity.java
+++ app/src/main/java/jarno/oussama/com/examenmonitor/StudentsListActivity.java
@@ -1,8 +1,8 @@
 package jarno.oussama.com.examenmonitor;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -25,16 +25,11 @@ public class StudentsListActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_students_list);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-
-            }
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener((View view) -> {
+            startActivity(new Intent(this, AddUserActivity.class));
         });
         StudentDatabase db = StudentDatabase.getDatabase(this);
         students = db.Instance.StudentDao().getAllStudents();
