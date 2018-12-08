9b8d470b006135019babc7023fe2e81076908db8
Jarno Cools <coolsjarno@gmail.com>
2018-12-08 19:06:21 +0100

    Refactored to androidX

--- app/src/main/java/jarno/oussama/com/examenmonitor/Database/Student.java
+++ app/src/main/java/jarno/oussama/com/examenmonitor/Database/Student.java
@@ -1,10 +1,10 @@
 package jarno.oussama.com.examenmonitor.Database;
 
-import android.arch.persistence.room.ColumnInfo;
-import android.arch.persistence.room.Entity;
-import android.arch.persistence.room.Index;
-import android.arch.persistence.room.PrimaryKey;
-import android.support.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Index;
+import androidx.room.PrimaryKey;
+import androidx.annotation.NonNull;
 
 @Entity(indices = {@Index(value = "card_id", unique = true)})
 public class Student {
