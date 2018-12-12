41b9569c8ddb9775f36412da0b6cddd74f3f9aa7
oussama Es-Salhi <oussie11@gmail.com>
2018-12-12 21:08:53 +0100

    switched from room database to Firebase DB

--- app/src/main/java/jarno/oussama/com/examenmonitor/MainActivity.java
+++ app/src/main/java/jarno/oussama/com/examenmonitor/MainActivity.java
@@ -5,12 +5,15 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
     }
 
     public void AddUserActivity(View view) {
